f8e3bd71c05c6b30409c9cc583972931c30132f6
KaiSLOAT <>
2022-05-04 16:21:05 +0000

    done

--- ws1.cpp
+++ ws1.cpp
@@ -4,10 +4,17 @@ using namespace std;
 // Part 1.1 Complete the node declaration
 struct Node
 {
+  Node(int num);
+  int data;
+  Node *next;
 };
 
 // Part 1.2 Define the Node constructor
 
+Node::Node(int num){
+  next = nullptr;
+  data = num;
+}
 //constants
 const int SENTINEL = -999;
 
@@ -24,7 +31,15 @@ int main()
     cin >> num;
     while (num != SENTINEL) {
         // Part 2, create a new node pointer, and append it to the list.
-    
+      Node *ptr = new Node(num);
+      if(tail == nullptr){
+        head = ptr;
+        tail = ptr;
+      }
+      else{
+        tail->next = ptr;
+        tail = ptr;
+      }
         cout << "Enter a number to add to the list (" << SENTINEL << " to end)";
         cin >> num;
     }
@@ -32,11 +47,22 @@ int main()
 	printList(head);
     
     // Part 3, delete the list
-
+    while(head != nullptr){
+      Node *remove = head->next;
+      delete head;
+      head = remove;
+    }
+    delete tail;
+    tail = nullptr;
 
     return 0;
 }
 
 // Part 4 fill in the print function to print the list
 void printList(Node *head) {
+  Node *ptr = head;
+  while(ptr != nullptr){
+    cout << ptr->data << endl;
+    ptr = ptr->next;
+  }
 }
